6a987b2408fad8c60adbf9e70981b969364e79d0
johndembaremba <59832682+John-Dembaremba@users.noreply.github.com>
2021-10-01 01:48:16 +0200

    Update proc.sol

--- contracts/proc.sol
+++ contracts/proc.sol
@@ -1,17 +1,25 @@
 pragma solidity >=0.7.0 <0.9.0;
 
-
-
 contract procurement {
-    
-    struct Supplier_details {
-        string[] name;
-        string[] zimraITF263;
-        string[] praz;
-        string[] validity;
+    uint256 public job_id = 0;
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
     }
-    
-    
+
+    struct Supplier_details {
+        uint256 job_id;
+        string name;
+        string price_unit;
+        string total_price;
+        string zimraITF263;
+        string praz;
+        string validity;
+    }
+
+    mapping(address => Supplier_details) public supplier;
+
     struct Client {
         string name;
         string product_type;
@@ -19,43 +27,131 @@ contract procurement {
         string requirements;
         string date;
         string delivery_period;
-        uint8 job;
-        mapping(uint8 => Supplier_details) suppliers;
+        uint256 job;
     }
-    
+
     mapping(address => Client) public transaction;
-    
-    function addTransaction(string memory _name, string memory _productType, string memory _product, string memory _req, string memory _date, string memory _del, uint8 _job) public {
-       
-        Client storage client = transaction[msg.sender];
+
+    event Jobs(
+        string name,
+        string product_type,
+        string product,
+        string requirements,
+        string date,
+        string delivery_period,
+        uint256 job
+    );
+
+    event Bidders(
+        uint256 job_id,
+        string name,
+        string price_unity,
+        string total_price,
+        string zimraITF263,
+        string praz,
+        string validity
+    );
+
+    event Winner(
+        uint256 job_id,
+        string name,
+        string price_unity,
+        string total_price,
+        string zimraITF263,
+        string praz,
+        string validity
+    );
+
+    mapping(address => uint256) public user;
+
+    function addTransaction(
+        string memory _name,
+        string memory _productType,
+        string memory _product,
+        string memory _req,
+        string memory _date,
+        string memory _del
+    ) public {
+        Client storage client = transaction[owner];
+        job_id++;
         client.name = _name;
         client.product_type = _productType;
         client.product = _product;
         client.requirements = _req;
-        client.date = _date;                
+        client.date = _date;
         client.delivery_period = _del;
-        client.job = _job;
+        client.job = job_id;
+
+        emit Jobs(_name, _productType, _product, _req, _date, _del, job_id);
     }
-    
-    function addSuppliers(uint8 _job,string[] memory _supNm, string[] memory _zimraITF263, string[] memory _praz, string[] memory _validity) public {
-        Client storage client = transaction[msg.sender];
-        client.suppliers[_job] = Supplier_details({name: _supNm, zimraITF263: _zimraITF263, praz: _praz, validity: _validity});
-        
+
+    function addSuppliers(
+        uint256 _job,
+        string memory _supNm,
+        string memory unit_price,
+        string memory total_price,
+        string memory _zimraITF263,
+        string memory _praz,
+        string memory _validity
+    ) public {
+        Supplier_details storage bid = supplier[owner];
+        bid.job_id = _job;
+        bid.name = _supNm;
+        bid.price_unit = unit_price;
+        bid.total_price = total_price;
+        bid.zimraITF263 = _zimraITF263;
+        bid.praz = _praz;
+        bid.validity = _validity;
+
+        emit Bidders(
+            _job,
+            _supNm,
+            unit_price,
+            total_price,
+            _zimraITF263,
+            _praz,
+            _validity
+        );
     }
-    
-    function getTransactions(address _address) view public returns(string memory, string memory, string memory, string memory, string memory, string memory,uint8) {
-        Client storage client = transaction[_address];
-        return (client.name, client.product_type, client.product, client.requirements, client.date, client.delivery_period, client.job);
+
+    //setJob_id maps user to job.id for the tender
+    function setJob_id(address _addr, uint256 _id) public {
+        user[_addr] = _id;
     }
-    
-    function getSuppliers(address _address, uint8 _job) view public returns(string[] memory, string[] memory, string[] memory, string[] memory) {
-        Client storage client = transaction[_address];
-        
-        return (client.suppliers[_job].name, client.suppliers[_job].zimraITF263, client.suppliers[_job].praz, client.suppliers[_job].validity);
+
+    // Exist() makes user to award bider only once by using user[]
+    modifier Exist(uint256 _id) {
+        require(user[owner] != _id, "Exist");
+        _;
+        setJob_id(owner, _id);
     }
-    
-    
-    
-    
-    
+
+    function award_bider(
+        uint256 _job,
+        string memory _supNm,
+        string memory unit_price,
+        string memory total_price,
+        string memory _zimraITF263,
+        string memory _praz,
+        string memory _validity
+    ) public Exist(_job) {
+        Supplier_details storage bid = supplier[owner];
+        bid.job_id = _job;
+        bid.name = _supNm;
+        bid.price_unit = unit_price;
+        bid.total_price = total_price;
+        bid.zimraITF263 = _zimraITF263;
+        bid.praz = _praz;
+        bid.validity = _validity;
+
+        emit Winner(
+            _job,
+            _supNm,
+            unit_price,
+            total_price,
+            _zimraITF263,
+            _praz,
+            _validity
+        );
+    }
 }
